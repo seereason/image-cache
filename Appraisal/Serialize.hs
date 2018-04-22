f2e5b9cacbd334f47f622dab4554b27fb18ea1f4
David Fox <dsf@seereason.com>
2018-04-22 08:59:08 -0700

    Bug in th-unify-clients fixed, switch back to my deriveSerialize

--- Appraisal/Serialize.hs
+++ Appraisal/Serialize.hs
@@ -1,10 +1,12 @@
 -- | Choose which deriveSerialize function to use everywhere.
 
 module Appraisal.Serialize
-    ( Data.THUnify.GHCGenerics.deriveSerialize
+    (
+      -- This one produces smaller code under ghcjs
+      -- Data.THUnify.GHCGenerics.deriveSerialize
+      -- This one compilers faster in many cases under ghcjs
+      Data.THUnify.Serialize.deriveSerialize
     ) where
 
--- This one produces smaller code under ghcjs
 import qualified Data.THUnify.Serialize (deriveSerialize)
--- This one compilers faster in many cases under ghcjs
 import qualified Data.THUnify.GHCGenerics (deriveSerialize)
