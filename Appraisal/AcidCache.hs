d22e1ccf9ca732ce6e6c2f5e73971a403e0158cb
David Fox <dsf@seereason.com>
2018-08-10 17:14:13 -0700

    Eliminate openValueCache, withValueCache is safer

--- Appraisal/AcidCache.hs
+++ Appraisal/AcidCache.hs
@@ -14,9 +14,8 @@
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 
 module Appraisal.AcidCache
-    ( -- * Cache declarations
-      openValueCache
-    , withValueCache
+    ( -- * Open cache
+      withAcidState
     -- * Cached map events
     , AcidVal
     , AcidKey
@@ -40,7 +39,7 @@ module Appraisal.AcidCache
 import Control.Exception (bracket)
 import Control.Monad.Reader (MonadReader(ask), ReaderT(runReaderT))
 import Control.Monad.State (liftIO, MonadIO, modify)
-import Data.Acid (AcidState, makeAcidic, openLocalStateFrom, Query, query, Update, update)
+import Data.Acid (AcidState, IsAcidic, makeAcidic, openLocalStateFrom, Query, query, Update, update)
 import Data.Acid.Local (createCheckpointAndClose)
 import Data.Generics (Proxy, Typeable)
 import Data.Map.Strict as Map (delete, difference, fromSet, insert, intersection, lookup, Map, union)
@@ -79,16 +78,16 @@ deleteValues keys = modify (`Map.difference` (Map.fromSet (const ()) keys))
 
 $(makeAcidic ''Map ['putValue, 'putValues, 'lookValue, 'lookValues, 'lookMap, 'deleteValue, 'deleteValues])
 
-initCacheMap :: Ord key => Map key val
-initCacheMap = mempty
+_initCacheMap :: Ord key => Map key val
+_initCacheMap = mempty
 
-openValueCache :: (AcidKey key, AcidVal val) =>
-                  FilePath -> IO (AcidState (Map key val))
-openValueCache path = openLocalStateFrom path initCacheMap
-
-withValueCache :: (AcidKey key, AcidVal val) =>
-                  FilePath -> (AcidState (Map key val) -> IO a) -> IO a
-withValueCache path f = bracket (openValueCache path) createCheckpointAndClose $ f
+withAcidState ::
+    (IsAcidic a, Typeable a)
+    => FilePath
+    -> a
+    -> (AcidState a -> IO r)
+    -> IO r
+withAcidState path initial f = bracket (openLocalStateFrom path initial) createCheckpointAndClose $ f
 
 -- | Class of monads for managing a key/value cache in acid state.
 -- The monad must be in MonadIO because it needs to query the acid
