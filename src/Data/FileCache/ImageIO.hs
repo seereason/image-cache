02a48213ac18ed02cf57068c1b8ab8a349b1078f
David Fox <dsf@seereason.com>
2025-12-09 13:21:07 -0800

    Switch back to old image scaler

--- src/Data/FileCache/ImageIO.hs
+++ src/Data/FileCache/ImageIO.hs
@@ -346,7 +346,7 @@ scaleImage' _ _ _ PDF = throwMember $ CannotScale PDF
 scaleImage' _ _ _ CSV = throwMember $ CannotScale CSV
 scaleImage' _ _ _ Unknown = throwMember $ CannotScale Unknown
 scaleImage' tmp sc input typ = do
-  if False
+  if True
   then do
     let decoder = case typ of
                     GIF -> showCommandForUser "giftopnm" ["-"]
