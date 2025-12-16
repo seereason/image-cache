c770c5d9ae8cdab2cbe77abc7cb1635fe38c1dac
David Fox <dsf@seereason.com>
2025-12-16 12:19:00 -0800

    Fix path to vips

--- src/Data/FileCache/ImageIO.hs
+++ src/Data/FileCache/ImageIO.hs
@@ -320,7 +320,7 @@ instance MakeByteString InputOutput where
 --     > readCreateProcessWithExitCode (vips_resize 0.5 "/home/dsf/Downloads/005832283_00146.jpg" "/tmp/out.jpg") ""
 --     (ExitSuccess,"","")
 vips_resize :: Double -> FilePath -> FilePath -> CreateProcess
-vips_resize sc fin fout = proc "vips" ["resize", fin, fout, showFFloat (Just 6) sc ""]
+vips_resize sc fin fout = proc "/run/current-system/sw/bin/vips" ["resize", fin, fout, showFFloat (Just 6) sc ""]
 
 -- | Build an image resized by decoding, applying pnmscale, and then
 -- re-encoding.  The new image inherits attributes of the old (other
