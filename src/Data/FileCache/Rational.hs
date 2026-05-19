072c01686beeeb1c79366a4710a93e271761d440
David Fox <dsf@seereason.com>
2026-05-19 16:33:42 -0700

    Changes for sr-utils-1.115

--- src/Data/FileCache/Rational.hs
+++ src/Data/FileCache/Rational.hs
@@ -33,7 +33,7 @@ import Language.Haskell.TH.Instances ()
 import Language.Haskell.TH.Lift as TH ()
 import Numeric (readSigned, readFloat, showSigned, showFFloat)
 import qualified Numeric (fromRat)
-import SeeReason.Log (compactStack, getStack)
+import Extra.SrcLoc (compactStack, getStack)
 import Web.Routes (PathInfo(..))
 
 -- * Rational
